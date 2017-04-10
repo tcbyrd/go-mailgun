30f2992c6f3584addb32b5e06663267c8a88fb6c
Tommy Byrd <tcbyrd@github.com>
2017-04-10 01:02:02 -0400

    Added HTTP server and moved mailgun code to `/mailer`

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"gopkg.in/mailgun/mailgun-go.v1"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from http server!")
+}
+
+func mailer(w http.ResponseWriter, r *http.Request) {
 	APIKey := "key-ee1103e4605570723f7b6287dd1391d8"
 	publicAPIKey := "pubkey-9fab2901bde6e9be903a0618e533dfef"
 	domain := "app015fce21413f416191c94899f21ea43c.mailgun.org"
@@ -22,5 +28,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+	fmt.Fprintf(w, "ID: %s Resp: %s\n", id, resp)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	fmt.Printf("Listening on port %s\n", port)
+	http.HandleFunc("/mailer", mailer)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
 }
